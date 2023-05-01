de443ea32b2139845475bb6ab368fdf33960f997
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-04-30 23:45:04 -0300

    feat: creates a clock engine to manage and display pointers

--- src/core/clock.js
+++ src/core/clock.js
@@ -1,36 +1,30 @@
 class Clock {
-  /**
-   * @type {Visor}
-   */
   visor;
-
-  /**
-   * @type {CanvasRenderingContext2D}
-   */
+  engine;
   ctx;
-
-  /**
-   * @type {Number}
-   */
   width;
-
-  /**
-   * @type {Number}
-   */
   height;
 
+  /**
+   *
+   * @param {CanvasRenderingContext2D} ctx The context that will render the clock
+   * @param {Number} width The clock width
+   * @param {Number} height The clock height
+   */
   constructor(ctx, width, height) {
     this.ctx = ctx;
     this.width = width;
     this.height = height;
 
     this.visor = new Visor(width / 4);
+    this.engine = new Engine(width, height);
   }
 
   animate = () => {
     this.ctx.clearRect(0, 0, this.width, this.height);
 
     this.visor.draw(this.ctx, this.width, this.height);
+    this.engine.run(this.ctx, this.width, this.height);
 
     requestAnimationFrame(this.animate);
   };
---
+++ src/core/engine.js
@@ -0,0 +1,68 @@
+class Engine {
+  hourPointer;
+  minutePointer;
+  secondPointer;
+
+  /**
+   *
+   * @param {Number} width
+   * @param {Number} height
+   */
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+
+    this.hourPointer = new Pointer("#000", width / 5, 5);
+    this.minutePointer = new Pointer("#000", width / 4, 3);
+    this.secondPointer = new Pointer("red", width / 4, 2);
+  }
+
+  /**
+   *
+   * @param {Date} date
+   * @returns The angle that the hour cursor will be drawed
+   */
+  getHourAngle(date) {
+    const hourAngle = date.getHours() * 30 - 90 + (date.getMinutes() * 6) / 12;
+    return (hourAngle * Math.PI) / 360;
+  }
+
+  /**
+   *
+   * @param {Date} date
+   * @returns The angle that the minute cursor will be drawed
+   */
+  getMinuteAngle(date) {
+    const minuteAngle =
+      date.getMinutes() * 6 - 90 + (date.getSeconds() * 6) / 60;
+    return (minuteAngle * Math.PI) / 360;
+  }
+
+  /**
+   *
+   * @param {Date} date
+   * @returns The angle that the seconds cursor will be drawed
+   */
+  getSecondAngle(date) {
+    const secondsAngle = date.getSeconds() * 6 - 90;
+    return (secondsAngle * Math.PI) / 360;
+  }
+
+  /**
+   *
+   * @param {CanvasRenderingContext2D} ctx The context to draw the engine peaces
+   * @param {Number} width The canvas context width
+   * @param {Number} height The canvas context height
+   */
+  run(ctx) {
+    ctx.save();
+    ctx.translate(this.width / 2, this.height / 2);
+
+    const date = new Date();
+    this.hourPointer.draw(ctx, this.getHourAngle(date));
+    this.minutePointer.draw(ctx, this.getMinuteAngle(date));
+    this.secondPointer.draw(ctx, this.getSecondAngle(date));
+
+    ctx.restore();
+  }
+}
